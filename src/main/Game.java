f9e8d82ee4dc532af0f80a7521f1ddd61d508ba5
hung <hungngo1310@gmail.com>
2023-12-18 02:36:02 +0700

    Fix scale issue

--- src/main/Game.java
+++ src/main/Game.java
@@ -19,10 +19,10 @@ public class Game implements Runnable {
     public static final int UPS = 200;
 
     public final static int TILES_DEFAULT_SIZE = 32;
-    public final static float SCALE = 1.5f;
+    public final static float SCALE = 1f;
     public final static int TILES_IN_WIDTH = 26;
     public final static int TILES_IN_HEIGHT = 14;
-    public final static int TILES_SIZE = (int) SCALE * TILES_DEFAULT_SIZE;
+    public final static int TILES_SIZE = (int) (SCALE * TILES_DEFAULT_SIZE);
     public final static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
     public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
     public Game() {
