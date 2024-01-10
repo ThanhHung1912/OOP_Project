9a07b17b7749d346d6b73a1268d7bcba4fe38a57
jenniferduong20012004 <jenniferduong20012004@gmail.com>
2024-01-10 22:18:28 +0700

    Add object class

--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -36,6 +36,8 @@ public class LoadSave {
     public static final String BIG_CLOUDS = "big_clouds.png";
     public static final String SMALL_CLOUDS = "small_clouds.png";
     public static final String COMPLETED_IMG = "completed_sprite.png";
+    public static final String CONTAINER_ATLAS = "objects_sprites.png";
+    public static final String POTION_ATLAS = "potions_sprites.png";
 
     public static BufferedImage getSpriteAtlas(String fileName) {
         BufferedImage img = null;
