3a5ede9425a0ee40de326be213664802a0a6114f
ThanhHung1912 <nguyenthanhung1912@gmail.com>
2024-01-12 00:40:21 +0700

    Press Z for attack

--- src/gameStates/Playing.java
+++ src/gameStates/Playing.java
@@ -216,6 +216,9 @@ public class Playing extends State implements Statemethods{
                 case KeyEvent.VK_SPACE:
                     player.setJump(true);
                     break;
+                case KeyEvent.VK_Z:
+                    player.setAttacking(true);
+                    break;
                 case KeyEvent.VK_BACK_SPACE:
                     Gamestate.state = Gamestate.MENU;
                     break;
@@ -237,6 +240,9 @@ public class Playing extends State implements Statemethods{
                     break;
                 case KeyEvent.VK_SPACE:
                     player.setJump(false);
+                    break;
+                case KeyEvent.VK_Z:
+                    player.setAttacking(false);
                     break;
             }
     }
