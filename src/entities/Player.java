870218b4637860c0eb0d7b9a6dd262cdbc2e961a
httgiang <httgiang.gdsciu@gmail.com>
2024-01-15 16:46:08 +0700

    Fix the bug regarding the Attack box

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -177,7 +177,13 @@ public class Player extends Entity {
 
     }
     private void updateAttackBox() {
-        if (right || (powerAttackActive && flipW == 1))
+        if (right && left){
+            if(flipW == 1){
+                attackBox.x = hitBox.x + hitBox.width + (int) (Game.SCALE * 10);
+            } else {
+                attackBox.x = hitBox.x - hitBox.width - (int) (Game.SCALE * 10);
+            }
+        } else if (right || (powerAttackActive && flipW == 1))
             attackBox.x = hitBox.x + hitBox.width + (int) (Game.SCALE * 10);
         else if (left || (powerAttackActive && flipW == -1))
             attackBox.x = hitBox.x - hitBox.width - (int) (Game.SCALE * 10);
@@ -231,19 +237,19 @@ public class Player extends Entity {
             }
         float xSpeed = 0;
 
-        if (left) {
+        if (left && !right) {
             xSpeed -= walkSpeed;
             flipX = width;
             flipW = -1;
         }
-        if (right) {
+        if (right && !left) {
             xSpeed += walkSpeed;
             flipX = 0;
             flipW = 1;
         }
 
         if(powerAttackActive){
-            if(!left && !right){
+            if((!left && !right) || (left && right)){
                 if(flipW == -1){
                     xSpeed = -walkSpeed;
                 } else {
