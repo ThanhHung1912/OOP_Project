68c954d82e1836bb33c2b426dc660102c48792a9
httgiang <httgiang.gdsciu@gmail.com>
2024-01-16 01:37:36 +0700

    Change the x value of Potions

--- src/object/Potion.java
+++ src/object/Potion.java
@@ -6,7 +6,7 @@ public class Potion extends GameObject{
     private float hoverOffset;
     private int maxHoverOffset, hoverDir = 1;
     public Potion(int x, int y, int objectType) {
-        super(x, (int) (y + 10 * Game.SCALE), objectType);
+        super((int) (x + 10 * Game.SCALE), (int) (y + 10 * Game.SCALE), objectType);
         doAnimation = true;
         initHitbox(7, 14);
         xDrawOffSet = (int)(3 * Game.SCALE);
